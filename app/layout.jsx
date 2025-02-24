3e45d0b728c4a13baeeaf15e563dd238ad61cf5c
Ochis27 <ochispaulcatalin27@gmail.com>
2025-02-24 13:26:48 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,15 +9,17 @@ export const metadata = {
 }
 
 const MainLayout = ({ children }) => {
-  return (<html>
-    <body>
-      <Navbar />
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </body>
-  </html>);
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
 
 export default MainLayout;
